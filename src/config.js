8637d276b8401d1552edfa1c5f2c0baad3ed27fd
Taylor Phelps <tphelps1993@gmail.com>
2021-01-13 14:03:30 -0500

    updated config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   OMDB_API_URL:
     process.env.OMDB_API_URL || 'http://www.omdbapi.com/?apikey=bd085d9a&s=',
-  API_BASE_URL: process.env.REACT_APP_BASE_URL || 'http://localhost:8000/api',
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
   TOKEN_KEY: 'hw-client-auth-token',
 };
